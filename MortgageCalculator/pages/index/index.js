7d92eca57eee60ba4c0979ba8673d33743f3a306
张晓宇 <zhangxiaoyu@91jinrong.com>
2018-08-02 18:42:34 +0800

    cell

--- MortgageCalculator/pages/index/index.js
+++ MortgageCalculator/pages/index/index.js
@@ -25,7 +25,18 @@ Page({
       secondContentDes: '等额本金',
       leftSelected: false,
       rightSelected: true
-    }]
+    }],
+    changeContentItem: {
+      changeContentTypeNameDes: '贷款比例:',
+      array: ['2成','2.5成','3成','3.5成','4成','4.5成','5成','5.5成','6成','6.5成','7成','7.5成','8成'],
+      index: 0
+    },
+    fenqiItem: {
+      changeContentTypeNameDes: '按揭年数:',
+      array: ['30年(360期)','29年(348期)','28年(336期)','27年(324期)','26年(312期)','25年(300期)','24年(228期)','23年(276期)','22年(264期)','21年(252期)','20年(240期)','19年(228期)','18年(216期)','17年(204期)','16年(192期)','15年(180期)','14年(168期)','13年(156期)','12年(144期)','11年(132期)','10年(120期)','9年(108期)','8年(96期)','7年(84期)','6年(72期)','5年(60期)','4年(48期)','3年(36期)','2年(24期)','1年(12期)'],
+      index: 0
+    }
+    
   },
   onLoad: function () {
   },
@@ -113,6 +124,13 @@ Page({
       }
         break;
     }
+  },
+  bindPickerChange(e) {
+    console.log(e);
+    console.log(e.detail.value);
 
+    this.setData({
+      'changeContentItem.index' : e.detail.value
+    });
   }
 })
